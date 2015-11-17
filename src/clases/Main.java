30e4e94aedb76e37e610796bf9e1aa50193927c2
Victor Hernandez <alu0100697032@ull.edu.es>
2015-11-17 12:17:55 +0000

    Added an instance of InfoProblema in class Main

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		InfoProblema problema = new InfoProblema();
+		problema.leerDatosProblema("src/ficherosXML/att48.xml");
 	}
 
 }
